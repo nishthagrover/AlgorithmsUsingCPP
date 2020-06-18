1e4a101be8433f868c8a6e3e4c851494a37059d0
Nishtha Grover <46939668+nishthagrover@users.noreply.github.com>
2020-06-18 08:52:11 +0530

    Linear Search
    Linear search implementation in C++ using Iterative and recursive approach.
    Linear Search: It is the simplest search algorithm with an time complexity of O(n) where n is the size of the array. This is a brute force approach where we traverse through the array and compare each element with the required search element. If they are equal, we return the index at which the match was found. If no match is found, we return -1 and indicate that the element wasn't present in the array.

---
+++ Searching-And-Sorting/LinearSearch.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+
+using namespace std;
+
+//if element is found i.e arr[i] equals to element
+// Function returns the index of the element in the array
+// if the element is not found, it returns -1
+
+//iterative approach
+int linearSearch(int *arr, int n, int element)
+{
+	for(int i=0; i<n; i++)
+	{
+		if(arr[i] == element)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+// Linear Search using Recursion
+int linearSearchRecursive(int *arr, int n, int element)
+{
+	if(n == -1)
+	{
+		return(-1);
+	}
+	if(arr[n-1] == element)
+		return n-1;
+	else
+		return linearSearchRecursive(arr, n-1, element);
+	
+}
+
+
+
+int main()
+{
+	
+	int n;
+	cout<<"Enter the size of array: ";
+	cin>>n;
+	
+	cout<<"\nEnter the elements\n";
+	int arr[n];
+	for (int i=0; i<n; i++)
+	{
+		cin>>arr[i];
+	}
+	
+	int element;
+	cout<<"Enter the element to search: ";
+	cin>>element;
+	
+	//linear search iterative
+	int index1 = linearSearch(arr, n, element);
+	index1 != -1? cout<<"\using Iterative approach\nElement is at index : "<< index1 : cout<<"\nElement not found.\n";
+	
+	//linear search recursive
+	int index2 = linearSearchRecursive(arr, n, element);
+	index2 != -1? cout<<"\using Recursive approach\nElement is at index : "<< index2 : cout<<"\nElement not found."<<endl;
+	
+	return 0;	
+}
